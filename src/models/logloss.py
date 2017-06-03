59644cc837191477fe72368bc04784c0d81c2827
Prithvi Nambiar <prithvinambiar@gmail.com>
2017-06-03 23:58:12 +0530

    renamed consistency.py file to logloss.py

--- src/models/logloss.py
+++ src/models/logloss.py
@@ -1,20 +1,11 @@
 __author__ = 'prithvin'
 
 import os
+
 from dotenv import find_dotenv, load_dotenv
 import matplotlib.pyplot as plt
-from math import *
-import numpy as np
 import pandas as pd
 from sklearn.metrics import log_loss
-
-# x = numpy.arange(0.00001, 5.0, 0.0001)
-# y = [log2(i) for i in x]
-#
-# print(y)
-# plt.style.use('ggplot')
-# plt.plot(x, y)
-# plt.show()
 
 
 def main():
@@ -26,6 +17,10 @@ def main():
     final = validation.merge(prediction, left_on='id', right_on='id', how='inner')
     log_loss_val = log_loss(final.target, final.probability)
     print('Log loss = ', log_loss_val)
+    plt.style.use('ggplot')
+    plt.plot(final.target, final.probability, 'ro')
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
